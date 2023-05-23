1938ad873f7ff13bacd41da017f62fd4c1a9558f
Luis Tapia <devlou@protonmail.com>
2023-05-22 23:10:36 -0500

    feat: add lexer tests and implementation

---
+++ tests/lexer_test.py
@@ -0,0 +1,25 @@
+from unittest import TestCase
+from typing import List
+
+from lang.token import (
+    Token,
+    TokenType,
+)
+from lang.lexer import Lexer
+
+class LexerTest(TestCase):
+    def test_illegal(self) -> None:
+        source: str = '¡¿@'
+        lexer: Lexer = Lexer(source)
+
+        tokens: List[Token] = []
+        for i in range(len(source)):
+            tokens.append(lexer.next_token())
+
+        expected_tokens: List[Token] = [
+            Token(TokenType.ILLEGAL, '¡'),
+            Token(TokenType.ILLEGAL, '¿'),
+            Token(TokenType.ILLEGAL, '@'),
+        ]
+
+        self.assertEqual(tokens, expected_tokens)
